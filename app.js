caac1a0724035ee1fc5bdd19cfc98d1f4d03cf90
Harry Ganz <hganz@rsmas.miami.edu>
2015-08-26 21:45:17 -0400

    Make a moving ball

--- app.js
+++ app.js
@@ -1,24 +1,32 @@
+/* Get the canvas and its context from the DOM */
 var canvas = document.getElementById('myCanvas');
 var ctx = canvas.getContext("2d");
 
-/* Draw a filled red rectangle at the specified vertices*/
-ctx.beginPath();
-ctx.rect(20, 40, 50, 50);
-ctx.fillStyle = "#FF0000";
-ctx.fill();
-ctx.closePath();
+/* starting ball position */
+var x = canvas.width/2;
+var y = canvas.height - 30;
+/* Ball change position after every frame */
+var dx = 2;
+var dy = -2;
 
-/* Draw an with center 240, 160 with begin angle of 20deg and end angle of
-*  0deg, of arc length 2pi (full cicle) and in the ccw direction (true) or cw (false) */
-ctx.beginPath();
-ctx.arc(240, 160, 20, 0, Math.PI*2, false);
-ctx.fillStyle = "green";
-ctx.fill();
-ctx.closePath();
+/* A function defining how to draw the ball */
+function draw() {
+  // Clear canvas before each frame
+  ctx.clearRect(0, 0, canvas.width, canvas.height);
+  drawBall();
+  x += dx;
+  y += dy;
+};
 
-/* A new rectangle with only stroke defined */
-ctx.beginPath();
-ctx.rect(160, 10, 100, 40);
-ctx.strokeStyle = "rgba(0, 0, 255, 0.5)";
-ctx.stroke();
-ctx.closePath();
+/* Draws a ball on the canvas */
+function drawBall() {
+  // Draw a circle at x, y
+  ctx.beginPath();
+  ctx.arc(x, y, 10, 0, Math.PI*2);
+  ctx.fillStyle = "#0095DD";
+  ctx.fill();
+  ctx.closePath();
+}
+
+/* Set a 10ms draw interval */
+setInterval(draw, 10);
