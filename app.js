5653771ed2272dcb9b9c3aad2a216538066db17c
Harry Ganz <hganz@rsmas.miami.edu>
2015-10-07 15:53:13 -0400

    Add collision detection

--- app.js
+++ app.js
@@ -31,7 +31,7 @@ var bricks = [];
 for (c=0; c<brickColumnCount; c++) {
   bricks[c] = [];
   for(r=0; r<brickRowCount; r++) {
-    bricks[c][r] = {x:0, y:0}
+    bricks[c][r] = {x:0, y:0, status: 1};
   }
 }
 
@@ -42,6 +42,7 @@ function draw() {
   drawBricks();
   drawBall();
   drawPaddle();
+  collisionDetection();
   // collision detection
   if(y + dy < ballRadius){
     dy = -dy;
@@ -83,15 +84,17 @@ function drawBall() {
 function drawBricks() {
   for(c=0; c<brickColumnCount; c++) {
     for(r=0; r<brickRowCount; r++) {
-      var brickX = (c*(brickWidth+brickPadding))+brickOffsetLeft;
-      var brickY = (r*(brickHeight+brickPadding))+brickOffsetTop;
-      bricks[c][r].x = brickX;
-      bricks[c][r].y = brickY;
-      ctx.beginPath();
-      ctx.rect(brickX, brickY, brickWidth, brickHeight);
-      ctx.fillStyle = "#0095DD";
-      ctx.fill();
-      ctx.closePath();
+      if(bricks[c][r].status === 1){
+        var brickX = (c*(brickWidth+brickPadding))+brickOffsetLeft;
+        var brickY = (r*(brickHeight+brickPadding))+brickOffsetTop;
+        bricks[c][r].x = brickX;
+        bricks[c][r].y = brickY;
+        ctx.beginPath();
+        ctx.rect(brickX, brickY, brickWidth, brickHeight);
+        ctx.fillStyle = "#0095DD";
+        ctx.fill();
+        ctx.closePath();
+      }
     }
   }
 }
@@ -124,5 +127,21 @@ function keyUpHandler(e) {
     leftPressed = false;
   }
 }
+
+/* Collision detection for bricks */
+function collisionDetection(){
+  for(c=0;c<brickColumnCount;c++){
+    for(r=0;r<brickRowCount;r++){
+      var b = bricks[c][r];
+      if(b.status === 1){
+        if(x > b.x && x< b.x+brickWidth && y > b.y && y < b.y+brickHeight){
+          dy = -dy;
+          b.status = 0;
+        }
+      }
+    }
+  }
+}
+
 /* Set a 10ms draw interval */
 setInterval(draw, 10);
